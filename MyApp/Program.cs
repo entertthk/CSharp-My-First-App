3bfc9b10ecc1e7bc10120c029eaba5c4e20a62ca
Opilane TTHK <opilane@tthk.ee>
2019-09-06 17:03:22 +0300

    my first commit

---
+++ MyApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Mis su nimi on?");
+            string name;
+
+            name = Console.ReadLine();
+            Console.WriteLine("Hello "  + name);
+              
+ 
+            Console.ReadLine();
+
+
+        }
+    }
+}
